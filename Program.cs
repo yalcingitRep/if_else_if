5fdf7ae6409c49fdc31478ada9d8fda75e30ede4
Yalcin <bilgekagan902@gmail.com>
2024-02-29 00:00:57 +0300

    if else if

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace if_else_if{
+    class Program{
+        public static void Main(string[] args){
+            int time = DateTime.Now.Hour;
+            if(time >= 6 && time <11){
+                Console.WriteLine("Günaydın!");
+            }
+            else if(time <= 18){
+                Console.WriteLine("iyi günler");
+            }
+            else{
+                Console.WriteLine("iyi geceler");
+            }
+            string sonuc = time<=18 ? "iyi günler!":"iyi geceler!";
+            Console.WriteLine(sonuc);
+
+            sonuc = time >= 6 && time<11 ? "Günaydın":time<=18 ? "iyi günler!":"iyi geceler!";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
